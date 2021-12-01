2dff9d2f207eb33aa073a0c61875811f46408e84
Abdullah Taha AKSAKAL <aaksakal050@gmail.com>
2021-12-01 16:19:51 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Abstract;
+using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car {Id=1, BrandId=1, ColorId=1, DailyPrice=500, ModelYear="2020", Description="Dizel Otomatik 1.5 Dci Clio"},
+                new Car {Id=2, BrandId=2, ColorId=2, DailyPrice=600, ModelYear="2020", Description="Dizel Manuel 1.5 Dci Megane"},
+                new Car {Id=3, BrandId=3, ColorId=2, DailyPrice=400, ModelYear="2019", Description="Dizel Otomatik 1.6 Tdi Polo"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(Car car)
+        {
+            return _cars.Where(c => c.Id == car.Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = 6;
+        }
+    }
+}
