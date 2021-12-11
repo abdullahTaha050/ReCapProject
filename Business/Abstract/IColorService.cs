3c8557fd9143793bb2233dbd4dcecf1789053348
Abdullah Taha AKSAKAL <aaksakal050@gmail.com>
2021-12-11 12:44:55 +0300

    9.gün eksik kısımları eklendi.

---
+++ Business/Abstract/IColorService.cs
@@ -0,0 +1,16 @@
+﻿using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IColorService
+    {
+        void Add(Color color);
+        void Update(Color color);
+        void Delete(Color color);
+        List<Color> GetAll();
+        List<Color> GetColorsByColorId(int colorId);
+    }
+}
