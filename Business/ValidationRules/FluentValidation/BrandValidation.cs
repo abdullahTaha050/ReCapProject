368ebbebba771b2c9e53dae00c0ebed7521c705c
Abdullah Taha AKSAKAL <aaksakal050@gmail.com>
2021-12-16 21:00:35 +0300

    12.gün ödev 2, fluent validation eklendi.

---
+++ Business/ValidationRules/FluentValidation/BrandValidation.cs
@@ -0,0 +1,17 @@
+﻿using Entites.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class BrandValidator : AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(b => b.BrandName).MinimumLength(3);
+            RuleFor(b => b.BrandName).NotNull();
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entites.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).MinimumLength(3);
+            RuleFor(u => u.FirstName).NotNull();
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).MinimumLength(3);
+            RuleFor(u => u.LastName).NotNull();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Password).NotNull();
+            RuleFor(u => u.Password).MinimumLength(8);
+        }
+    }
+}
